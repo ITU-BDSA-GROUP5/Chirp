861fd98573239f7452d26579a8b828868eceed1a
Niklas <nizc@itu.dk>
2023-08-31 15:37:10 +0200

    Add cheep creation functionality Co-authored-by: Philipnah <philip@han.dk>, jakobArnfred <arni@itu.dk>

--- Program.cs
+++ Program.cs
@@ -1,9 +1,9 @@
-﻿if (args.Length > 1)
+﻿if (args.Length < 1)
     Console.WriteLine("Please, enter read to read or cheep to cheep");
 else if (args[0] == "read")
     Read();
 else if (args[0] == "cheep")
-    Cheep();
+    Cheep(args[1]);
 
 
 static void Read()
@@ -11,7 +11,19 @@ static void Read()
     return;
 }
 
-static void Cheep()
+static void Cheep(string message)
 {
+    string user = Environment.UserName;
+    var time = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+    if(!File.Exists("chirp_cli_db2.csv"))
+    {
+        using StreamWriter sw = File.CreateText("chirp_cli_db.csv");
+        sw.WriteLine($"{user},{message},{time}");
+    }
+    else
+    {
+        using StreamWriter sw = File.AppendText("chirp_cli_db.csv");
+        sw.WriteLine($"{user},{message},{time}");
+    }
     return;
 }
