5838fcc4386b642344d74645e26cec6fd0dd57f8
johan <johbr@itu.dk>
2023-09-02 12:29:26 +0200

    implement read functionality
    Co-authored-by: Olivier-Baptiste Hansen

--- Program.cs
+++ Program.cs
@@ -1,14 +1,32 @@
-﻿if (args.Length > 1)
+﻿using System.Text.RegularExpressions;
+
+if (args.Length > 1)
     Console.WriteLine("Please, enter read to read or cheep to cheep");
 else if (args[0] == "read")
     Read();
 else if (args[0] == "cheep")
     Cheep();
 
-
+//Prints all past cheeps
 static void Read()
 {
-    return;
+    using (StreamReader reader = File.OpenText("chirp_cli_db.csv"))
+    {
+        string cheep;
+        reader.ReadLine();
+        while ((cheep = reader.ReadLine()) is not null)
+        {
+            Console.WriteLine(FormatCheep(cheep));
+        }
+    }
+}
+
+static string FormatCheep(string cheep){
+    Regex cheepSegmentRegex = new Regex(@"(?<user>[a-zA-Z]+),""(?<cheep>.+)"",(?<timestamp>[0-9]+)");
+    GroupCollection cheepSegments = cheepSegmentRegex.Matches(cheep)[0].Groups;
+    DateTimeOffset timestamp = DateTimeOffset.FromUnixTimeSeconds(long.Parse(cheepSegments["timestamp"].Value));
+    
+    return $"{cheepSegments["user"].Value} @ {timestamp}: {cheepSegments["cheep"].Value}";
 }
 
 static void Cheep()
