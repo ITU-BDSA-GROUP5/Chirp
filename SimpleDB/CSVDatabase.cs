c2437b8b037a1a373de2edd677ebe35325a94247
Olivier-Baptiste Hansen <oliha@itu.dk>
2023-09-07 17:07:49 +0200

    Implement write method using CsvHelper
    Co-authored-by: Johan Brandi <johbr@itu.dk>

--- SimpleDB/CSVDatabase.cs
+++ SimpleDB/CSVDatabase.cs
@@ -1,5 +1,7 @@
 ﻿namespace SimpleDB;
 
+using System.Globalization;
+using CsvHelper;
 
 public class CSVDatabase<T> : IDatabaseRepository<T>
 {
@@ -11,7 +13,7 @@ public class CSVDatabase<T> : IDatabaseRepository<T>
     public IEnumerable<T> Read(int? limit = null)
     {
         using (var reader = new StreamReader(CSVPath))
-        using (var csv = new CsvReader(reader))
+        using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
         {
             return csv.GetRecords<T>();
         }
@@ -19,6 +21,10 @@ public class CSVDatabase<T> : IDatabaseRepository<T>
 
     public void Store(T record)
     {
-        
+        using (var writer = new StreamWriter(CSVPath))
+        using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+        {
+            csv.WriteRecord(record);
+        }
     }
 }
