d9340c022370e87663b0e2c1819a6fa6a0bdcf5c
Olivier-Baptiste Hansen <oliha@itu.dk>
2023-09-07 16:57:34 +0200

    Implement read method using CsvHelper
    Co-authored-by: Johan Brandi <johbr@itu.dk>

--- SimpleDB/CSVDatabase.cs
+++ SimpleDB/CSVDatabase.cs
@@ -1,6 +1,24 @@
 ﻿namespace SimpleDB;
 
-public class SimpleDB
-{
 
+public class CSVDatabase<T> : IDatabaseRepository<T>
+{
+    private string CSVPath;
+    public CSVDatabase(string csvPath)
+    {
+        this.CSVPath = csvPath;
+    }
+    public IEnumerable<T> Read(int? limit = null)
+    {
+        using (var reader = new StreamReader(CSVPath))
+        using (var csv = new CsvReader(reader))
+        {
+            return csv.GetRecords<T>();
+        }
+    }
+
+    public void Store(T record)
+    {
+        
+    }
 }
