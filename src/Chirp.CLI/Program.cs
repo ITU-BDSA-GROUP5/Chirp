ddab5622485d420ed3977e5bffca585e37b42ffd
Philip Han <46556419+Philipnah@users.noreply.github.com>
2023-09-19 13:43:37 +0200

    Add release.yml file, change .csproj include .csv
    Co-authored-by: Niklas Christensen
    Co-authored-by: JakobArnfred

--- src/Chirp.CLI/Program.cs
+++ src/Chirp.CLI/Program.cs
@@ -7,35 +7,36 @@ using DocoptNet;
 string usage = @"
 Usage:
 	chirp <command> [<message>]";
-	
+
 // This line was taken from chatgpt
 var arguments = new Docopt().Apply(usage, args, version: "Chirp 1.0", exit: true);
 
-if (arguments is null){throw new ArgumentNullException(nameof(arguments)); }
+if (arguments is null) { throw new ArgumentNullException(nameof(arguments)); }
 string input = arguments["<command>"].ToString();
 
 if (input == "read")
-    Read();
+	Read();
 else if (input == "cheep")
 	Cheep(arguments["<message>"].ToString());
 
 static void Read()
 {
-    IDatabaseRepository<Cheep> db = GetDB();
-    IEnumerable<Cheep> cheeps = db.Read();
+	IDatabaseRepository<Cheep> db = GetDB();
+	IEnumerable<Cheep> cheeps = db.Read();
 
 	PrintCheeps(cheeps);
 }
 
 static void Cheep(string message)
 {
-    IDatabaseRepository<Cheep> db = GetDB();
-    string author = Environment.UserName;
-    var time = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+	IDatabaseRepository<Cheep> db = GetDB();
+	string author = Environment.UserName;
+	var time = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
 
-    db.Store(new Cheep(author, message, time));
+	db.Store(new Cheep(author, message, time));
 }
 
-static IDatabaseRepository<Cheep> GetDB() {
-    return new CSVDatabase<Cheep>("./chirp_cli_db.csv");
+static IDatabaseRepository<Cheep> GetDB()
+{
+	return new CSVDatabase<Cheep>("chirp_cli_db.csv");
 }
