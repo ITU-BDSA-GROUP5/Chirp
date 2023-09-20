8dbc9763231b2882a6c3465d94af4b9a88648f6d
SnowyPeax <johbr@itu.dk>
2023-09-20 16:59:06 +0200

    commit to transfer to OLIHA's device

---
+++ test/Chirp.CLI.Tests/UserInterfaceTests.cs
@@ -0,0 +1,19 @@
+namespace Chirp.CLI.Tests;
+
+public class UserInterfaceTests
+{
+
+    [Theory]
+    [InlineData()]
+    public void FormatCheep_Cheeps_ReturnsProperString(Cheep cheep, string expectation)
+    {
+        // Arrange
+        string CheepText = ""
+
+        // Act
+
+
+        // Assert
+    }
+
+}
