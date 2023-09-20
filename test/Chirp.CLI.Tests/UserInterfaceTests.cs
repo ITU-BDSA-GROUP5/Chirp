6cfe1aadd4346896e6c1457d482075b00d0cee8f
johan <johbr@itu.dk>
2023-09-20 17:32:53 +0200

    write test to check formatting of cheeps
    Note: fails due to type or namespace 'Cheep' not being found

--- test/Chirp.CLI.Tests/UserInterfaceTests.cs
+++ test/Chirp.CLI.Tests/UserInterfaceTests.cs
@@ -4,16 +4,16 @@ public class UserInterfaceTests
 {
 
     [Theory]
-    [InlineData()]
+    [InlineData(new Cheep("apple","Hello world",1694437331), "apple @ 11/09/2023 15:02:11: Hello world")]
     public void FormatCheep_Cheeps_ReturnsProperString(Cheep cheep, string expectation)
     {
         // Arrange
-        string CheepText = ""
+        string CheepText = "";
 
         // Act
-
+        CheepText = UserInterface.FormatCheep(cheep);
 
         // Assert
+        Assert.Equal(expectation, CheepText);
     }
-
 }
