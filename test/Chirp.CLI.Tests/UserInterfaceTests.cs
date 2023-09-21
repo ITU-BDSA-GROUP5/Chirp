fe31c039a1c46933b9603be8c32c5f40479af0a9
Olivier-Baptiste Hansen <oliha@itu.dk>
2023-09-21 16:04:18 +0200

    Add reference to test project
    Co-authored-by: Johan <johbr@itu.dk>

--- test/Chirp.CLI.Tests/UserInterfaceTests.cs
+++ test/Chirp.CLI.Tests/UserInterfaceTests.cs
@@ -2,7 +2,6 @@ namespace Chirp.CLI.Tests;
 
 public class UserInterfaceTests
 {
-
     [Theory]
     [InlineData(new Cheep("apple","Hello world",1694437331), "apple @ 11/09/2023 15:02:11: Hello world")]
     public void FormatCheep_Cheeps_ReturnsProperString(Cheep cheep, string expectation)
