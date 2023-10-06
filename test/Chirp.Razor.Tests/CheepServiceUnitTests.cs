f84d50b47a0cc022e8afebd1847ac15d14396a73
Philip Han <46556419+Philipnah@users.noreply.github.com>
2023-10-06 16:32:41 +0200

    Fix page number & formatting on unit tests

--- test/Chirp.Razor.Tests/CheepServiceUnitTests.cs
+++ test/Chirp.Razor.Tests/CheepServiceUnitTests.cs
@@ -1,40 +1,37 @@
 namespace Chirp.Razor.Tests;
 
-using Xunit;
-using Chirp.Razor;
-
 public class CheepServiceUnitTests
 {
-  private readonly ICheepService _cheepService;
+	private readonly ICheepService _cheepService;
 
-  public CheepServiceUnitTests()
-  {
-    _cheepService = new CheepService();
-  }
+	public CheepServiceUnitTests()
+	{
+		_cheepService = new CheepService();
+	}
 
-  [Fact]
-  public void Read_CheepNotNull()
-  {
-    // Arrange
-    var cheeps = _cheepService.GetCheeps(0);
+	[Fact]
+	public void Read_CheepNotNull()
+	{
+		// Arrange
+		var cheeps = _cheepService.GetCheeps(1);
 
-    // Act
-    CheepViewModel? cheep = cheeps.FirstOrDefault();
+		// Act
+		CheepViewModel? cheep = cheeps.FirstOrDefault();
 
-    // Assert
-    Assert.NotNull(cheep);
-  }
+		// Assert
+		Assert.NotNull(cheep);
+	}
 
-  [Fact]
-  public void Read_CheepsDescendingOrder()
-  {
-    // Arrange
-    var cheeps = _cheepService.GetCheeps(0);
+	[Fact]
+	public void Read_CheepsDescendingOrder()
+	{
+		// Arrange
+		var cheeps = _cheepService.GetCheeps(1);
 
-    // Act
-    var orderedCheeps = _cheepService.GetCheeps(0).OrderByDescending(cheep => cheep.Timestamp);
+		// Act
+		var orderedCheeps = _cheepService.GetCheeps(1).OrderByDescending(cheep => cheep.Timestamp);
 
-    // Assert
-    Assert.Equal(cheeps, orderedCheeps);
-  }
+		// Assert
+		Assert.Equal(cheeps, orderedCheeps);
+	}
 }
