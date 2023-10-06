c449149fd79c3a76b952a1d27cb62f8eae82cea1
Philip Han <46556419+Philipnah@users.noreply.github.com>
2023-10-06 12:07:46 +0200

    data folder copies to output & tests work!
    Co-authored-by: Olivier <oliha@itu.dk>
    Co-authored-by: Johan <johbr@itu.dk>

--- test/Chirp.Razor.Tests/ChirpRazorIntegrationTests.cs
+++ test/Chirp.Razor.Tests/ChirpRazorIntegrationTests.cs
@@ -1,39 +1,39 @@
 
-// using Microsoft.VisualStudio.TestPlatform.TestHost;
-// using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Microsoft.AspNetCore.Mvc.Testing;
 
-// public class TestAPI : IClassFixture<WebApplicationFactory<Program>>
-// {
-//     private readonly WebApplicationFactory<Program> _fixture;
-//     private readonly HttpClient _client;
+public class TestAPI : IClassFixture<WebApplicationFactory<Program>>
+{
+	private readonly WebApplicationFactory<Program> _fixture;
+	private readonly HttpClient _client;
 
-//     public TestAPI(WebApplicationFactory<Program> fixture)
-//     {
-//         _fixture = fixture;
-//         _client = _fixture.CreateClient(new WebApplicationFactoryClientOptions { AllowAutoRedirect = true, HandleCookies = true });
-//     }
+	public TestAPI(WebApplicationFactory<Program> fixture)
+	{
+		_fixture = fixture;
+		_client = _fixture.CreateClient(new WebApplicationFactoryClientOptions { AllowAutoRedirect = true, HandleCookies = true });
+	}
 
-//     [Fact]
-//     public async void CanSeePublicTimeline()
-//     {
-//         var response = await _client.GetAsync("/");
-//         response.EnsureSuccessStatusCode();
-//         var content = await response.Content.ReadAsStringAsync();
+	[Fact]
+	public async void CanSeePublicTimeline()
+	{
+		var response = await _client.GetAsync("/");
+		response.EnsureSuccessStatusCode();
+		var content = await response.Content.ReadAsStringAsync();
 
-//         Assert.Contains("Chirp!", content);
-//         Assert.Contains("Public Timeline", content);
-//     }
+		Assert.Contains("Chirp!", content);
+		Assert.Contains("Public Timeline", content);
+	}
 
-//     [Theory]
-//     [InlineData("Helge")]
-//     [InlineData("Rasmus")]
-//     public async void CanSeePrivateTimeline(string author)
-//     {
-//         var response = await _client.GetAsync($"/{author}");
-//         response.EnsureSuccessStatusCode();
-//         var content = await response.Content.ReadAsStringAsync();
+	[Theory]
+	[InlineData("Helge")]
+	[InlineData("Rasmus")]
+	public async void CanSeePrivateTimeline(string author)
+	{
+		var response = await _client.GetAsync($"/{author}");
+		response.EnsureSuccessStatusCode();
+		var content = await response.Content.ReadAsStringAsync();
 
-//         Assert.Contains("Chirp!", content);
-//         Assert.Contains($"{author}'s Timeline", content);
-//     }
-// }
+		Assert.Contains("Chirp!", content);
+		Assert.Contains($"{author}'s Timeline", content);
+	}
+}
