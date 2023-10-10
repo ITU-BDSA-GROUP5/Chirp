0e172a478aef1b29db026aae75dbd49acb45761c
Jakob Arnfred Nielsen <arni@itu.dk>
2023-10-10 14:17:33 +0200

    Basic CheepRepository implemented. Co-authored-by: Niklas <nizc@itu.dk

--- src/Chirp.Razor/Repositories/ICheepRepository.cs
+++ src/Chirp.Razor/Repositories/ICheepRepository.cs
@@ -2,5 +2,28 @@
 {
     public interface ICheepRepository
     {
+        public List<CheepViewModel> GetCheeps(int page);
+        public List<CheepViewModel> GetCheepsFromAuthor(int page, string author);
+    }
+
+    public class CheepRepository : ICheepRepository
+    {
+        private IDBFacade dbfacade;
+        private int pageSize = 32;
+
+        public CheepRepository()
+        {
+            dbfacade = DBFacade.GetInstance();
+        }
+
+        public List<CheepViewModel> GetCheeps(int page)
+        {
+            return dbfacade.Fetch(pageSize * (page - 1), pageSize * page);
+        }
+
+        public List<CheepViewModel> GetCheepsFromAuthor(int page, string author)
+        {
+            return dbfacade.FetchFromAuthor(pageSize * (page - 1), pageSize * page, author);
+        }
     }
 }
