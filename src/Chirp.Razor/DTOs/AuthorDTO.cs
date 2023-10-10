345250fe93689bc3870737011efd60eb7b694cc4
Olivier-Baptiste Hansen <oliha@itu.dk>
2023-10-10 10:59:16 +0200

    Establish DTOs

---
+++ src/Chirp.Razor/DTOs/AuthorDTO.cs
@@ -0,0 +1,5 @@
+public class AuthorDTO
+{
+    public int AuthorId { get; set; }
+    public string Name { get; set; }
+}
---
+++ src/Chirp.Razor/DTOs/CheepDTO.cs
@@ -0,0 +1,8 @@
+public class CheepDTO
+{
+    public int CheepId { get; set; }
+    public int AuthorId { get; set; }
+    public string AuthorName { get; set; }
+    public string Message { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
