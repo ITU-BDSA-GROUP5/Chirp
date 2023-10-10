8025581de5402ada1a41926d20e25d0f97adb536
Jakob Arnfred Nielsen <arni@itu.dk>
2023-10-10 15:08:55 +0200

    Initial commit not working. Co-authored-by: Philip <phgp@itu.dk> Co-authored-by: Niklas <nizc@itu.dk>

--- src/Chirp.Razor/Program.cs
+++ src/Chirp.Razor/Program.cs
@@ -4,7 +4,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-builder.Services.AddSingleton<ICheepService, CheepService>();
+//builder.Services.AddSingleton<ICheepService, CheepService>();
+//builder.Services.AddScoped<ICheepRepository, CheepRepository>();
 builder.Services.AddScoped<ICheepRepository, CheepRepository>();
 
 
--- src/Chirp.Razor/Repositories/ICheepRepository.cs
+++ src/Chirp.Razor/Repositories/ICheepRepository.cs
@@ -8,17 +8,25 @@
 
     public class CheepRepository : ICheepRepository
     {
-        private IDBFacade dbfacade;
+        //private IDBFacade dbfacade;
+        private readonly ChirpDBContext _DBContext;
         private int pageSize = 32;
 
-        public CheepRepository()
+        public CheepRepository(ChirpDBContext dBContext)
         {
             dbfacade = DBFacade.GetInstance();
+            _DBContext = dBContext;
         }
 
         public List<CheepViewModel> GetCheeps(int page)
         {
-            return dbfacade.Fetch(pageSize * (page - 1), pageSize * page);
+            var cheeps = _DBContext.Cheeps.Select(c => c);
+            var list = new List<Cheep>();
+            foreach (var cheep in cheeps) {
+                list.Add(cheep);
+            }
+            return list;
+            //return dbfacade.Fetch(pageSize * (page - 1), pageSize * page);
         }
 
         public List<CheepViewModel> GetCheepsFromAuthor(int page, string author)
