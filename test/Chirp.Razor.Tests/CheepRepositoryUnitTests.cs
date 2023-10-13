75450ae53fc3cbee8f26406c285f374a770c3c50
Philip Han <46556419+Philipnah@users.noreply.github.com>
2023-10-13 15:52:50 +0200

    use constructor & Dispose method for tests
    Also add new count test to check page size

--- test/Chirp.Razor.Tests/CheepRepositoryUnitTests.cs
+++ test/Chirp.Razor.Tests/CheepRepositoryUnitTests.cs
@@ -1,48 +1,46 @@
 namespace Chirp.Razor.Tests;
+
+using System.Diagnostics;
+
 using Chirp.Razor.Repositories;
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 
 public class CheepRepositoryUnitTests
 {
-	// private readonly ICheepRepository _cheepRepository;
-	// private readonly SqliteConnection _connection;
+	private readonly ICheepRepository _cheepRepository;
+	private readonly SqliteConnection _connection;
 
 	public CheepRepositoryUnitTests()
 	{
+		// Adapted from: https://learn.microsoft.com/en-us/ef/core/testing/testing-without-the-database#sqlite-in-memory
+
 		// Create and open a connection. This creates the SQLite in-memory database, which will persist until the connection is closed
 		// at the end of the test (see Dispose below).
-		// _connection = new SqliteConnection("Filename=:memory:");
-		// _connection.Open();
+		_connection = new SqliteConnection("Filename=:memory:");
+		_connection.Open();
 
-		// // These options will be used by the context instances in this test suite, including the connection opened above.
-		// var contextOptions = new DbContextOptionsBuilder<ChirpDBContext>()
-		// 	.UseSqlite(_connection)
-		// 	.Options;
+		// These options will be used by the context instances in this test suite, including the connection opened above.
+		var contextOptions = new DbContextOptionsBuilder<ChirpDBContext>()
+			.UseSqlite(_connection)
+			.Options;
 
-		// // Create the schema and seed some data
-		// using var context = new ChirpDBContext(contextOptions);
+		// Create the schema and seed some data
+		var context = new ChirpDBContext(contextOptions);
 
-		// _cheepRepository = new CheepRepository(context);
+		context.Database.EnsureCreated();
+		DbInitializer.SeedDatabase(context);
 
+		_cheepRepository = new CheepRepository(context);
 	}
 
-	// public void Dispose() => _connection.Dispose();
+	public void Dispose() => _connection.Dispose();
 
 	[Fact]
 	public void Read_CheepNotNull()
 	{
 		// Arrange
-		using var connection = new SqliteConnection("Filename=:memory:");
-		connection.Open();
-		var builder = new DbContextOptionsBuilder<ChirpDBContext>().UseSqlite(connection);
-		using var context = new ChirpDBContext(builder.Options);
-		context.Database.EnsureCreated();
-		DbInitializer.SeedDatabase(context);
-
-		var repository = new CheepRepository(context);
-
-		var cheeps = repository.GetCheeps(1);
+		var cheeps = _cheepRepository.GetCheeps(1);
 
 		// Act
 		CheepViewModel? cheep = cheeps.FirstOrDefault();
@@ -55,21 +53,25 @@ public class CheepRepositoryUnitTests
 	public void Read_CheepsDescendingOrder()
 	{
 		// Arrange
-		using var connection = new SqliteConnection("Filename=:memory:");
-		connection.Open();
-		var builder = new DbContextOptionsBuilder<ChirpDBContext>().UseSqlite(connection);
-		using var context = new ChirpDBContext(builder.Options);
-		context.Database.EnsureCreated();
-		DbInitializer.SeedDatabase(context);
-
-		var repository = new CheepRepository(context);
-
-		var cheeps = repository.GetCheeps(1);
+		var cheeps = _cheepRepository.GetCheeps(1);
 
 		// Act
-		var orderedCheeps = repository.GetCheeps(1).OrderByDescending(cheep => cheep.Timestamp);
+		var orderedCheeps = _cheepRepository.GetCheeps(1).OrderByDescending(cheep => cheep.Timestamp);
 
 		// Assert
 		Assert.Equal(cheeps, orderedCheeps);
+	}
+
+	[Fact]
+	public void Read_CheepPageSize()
+	{
+		// Arrange
+		int pageSize = 32;
+
+		// Act
+		var publicTimelineCheepCount = _cheepRepository.GetCheeps(1).Count;
+
+		// Assert
+		Assert.Equal(pageSize, publicTimelineCheepCount);
 	}
 }
