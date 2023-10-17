b9fe9b09a607ca6465adcd99156d81cd6335e22c
Jakob Arnfred Nielsen <arni@itu.dk>
2023-10-18 00:49:15 +0200

    AuthorRepository created
    AuthorRepository contains a constructor and has empty functions
    to implement the IAuthorRepository interface

--- src/Chirp.Razor/Repositories/AuthorRepository.cs
+++ src/Chirp.Razor/Repositories/AuthorRepository.cs
@@ -7,7 +7,28 @@
         public Author createNewAuthor(string name, string email);
     }
 
-    public class AuthorRepository
+    public class AuthorRepository : IAuthorRepository
     {
+        private readonly ChirpDBContext _context;
+
+        public AuthorRepository(ChirpDBContext context)
+        {
+            _context = context;
+        }
+
+        public Author createNewAuthor(string name, string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Author getAuthorByEmail(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Author getAuthorByName(string name)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
