7f57fa92d3048743f9bf2712c7dcf2222da28edd
johan <johbr@itu.dk>
2023-11-09 16:24:55 +0100

    add logic to onpost for sending cheeps
    Co-authored-by: Philip <phgp@itu.dk>
    Co-authored-by: Olivier-Baptiste <oliha@itu.dk>

--- src/Chirp.Web/Pages/Private/SendCheep.cshtml.cs
+++ src/Chirp.Web/Pages/Private/SendCheep.cshtml.cs
@@ -1,3 +1,5 @@
+using System.Security.Claims;
+using Chirp.Core;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,11 +11,46 @@ namespace Chirp.Web.Pages
         [BindProperty]
         public string? CheepMessage { get; set; }
 
+        private readonly IAuthorRepository AuthorRepository;
+        private readonly ICheepRepository CheepRepository;
+
+        public SendCheepModel(IAuthorRepository authorRepository, ICheepRepository cheepRepository)
+        {
+            AuthorRepository = authorRepository;
+            CheepRepository = cheepRepository;
+        }
+
         public IActionResult OnPost()
         {
-            Console.WriteLine("user wrote: " + CheepMessage);
+            string email = User.Claims.Where(a => a.Type == "emails").Select(e => e.Value).Single();
+
+            Console.WriteLine(email + " wrote: " + CheepMessage);
+
+            // check if author exists
+            // fetch author
+            // possibly create author
+
+            AuthorDTO? Author = AuthorRepository.GetAuthorByEmail(email).First();
+
+            if (Author == null)
+            {
+                Author = CreateNewAuthor();
+            }
+
+            CreateCheepDTO cheep = new CreateCheepDTO()
+            {
+                CheepGuid = new Guid(),
+                Text = CheepMessage,
+                Name = Author.Name,
+                Email = Author.Email
+            };
 
             return Redirect("/");
+        }
+
+        private AuthorDTO CreateNewAuthor()
+        {
+            throw new NotImplementedException();
         }
     }
 }
