8e5d1dce8a51e66b1c7879e58ff8236c068727d3
johan <johbr@itu.dk>
2023-11-09 15:25:55 +0100

    remove superfluous OnGet method
    Co-authored-by: Philip <phgp@itu.dk>

--- src/Chirp.Web/Pages/Private/SendCheep.cshtml.cs
+++ src/Chirp.Web/Pages/Private/SendCheep.cshtml.cs
@@ -3,23 +3,17 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Chirp.Web.Pages
 {
-	public class SendCheepModel : PageModel
-	{
+    public class SendCheepModel : PageModel
+    {
 
-		[BindProperty]
-		public string? CheepMessage { get; set; }
+        [BindProperty]
+        public string? CheepMessage { get; set; }
 
-		public IActionResult OnGet()
-		{
-			CheepMessage = "Brevity is the soul of the wit.";
-			Console.WriteLine(CheepMessage);
-			return Page();
-		}
+        public IActionResult OnPost()
+        {
+            Console.WriteLine("user wrote: " + CheepMessage);
 
-		public IActionResult OnPost()
-		{
-			Console.WriteLine(CheepMessage);
-			return Redirect("https://localhost:7102/");
-		}
-	}
+            return Redirect("/");
+        }
+    }
 }
