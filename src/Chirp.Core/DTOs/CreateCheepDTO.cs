57c1af038bca58db9d70e77d10580ad7774c84f1
johan <johbr@itu.dk>
2023-11-11 12:13:33 +0100

    remove superfluous values from CreateCheepDTO

--- src/Chirp.Core/DTOs/CreateCheepDTO.cs
+++ src/Chirp.Core/DTOs/CreateCheepDTO.cs
@@ -2,7 +2,5 @@ public class CreateCheepDTO
 {
 	public Guid CheepGuid { get; set; }
 	public required string Text { get; set; }
-	public required int AuthorId { get; set; }
-	public required string Name { get; set; }
 	public required string Email { get; set; }
 }
