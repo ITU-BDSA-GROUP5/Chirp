88d92b6e2d801674526ed1e80af8975f209b4ca6
johan <johbr@itu.dk>
2023-11-21 16:00:53 +0100

    add validator to CheepDTO

--- src/Chirp.Core/DTOs/CheepDTO.cs
+++ src/Chirp.Core/DTOs/CheepDTO.cs
@@ -1,6 +1,22 @@
+using FluentValidation;
+
 public class CheepDTO
 {
 	public required string AuthorName { get; set; }
 	public required string Message { get; set; }
 	public required string TimeStamp { get; set; }
+}
+
+public class CheepDTOValidator : AbstractValidator<CheepDTO>
+{
+	public CheepDTOValidator()
+	{
+		RuleFor(x => x.Message)
+			.Length(1, 160)
+			.WithMessage("Message is of invalid size");
+
+		RuleFor(x => x.TimeStamp)
+			.Matches("^[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}$")
+			.WithMessage("Timestamp of undesirable format");
+	}
 }
