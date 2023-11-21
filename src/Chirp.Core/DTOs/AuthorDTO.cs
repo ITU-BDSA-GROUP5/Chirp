712f969317e310af4b098986f95d51485a873d2b
johan <johbr@itu.dk>
2023-11-21 15:41:57 +0100

    add validator til authorDTO

--- src/Chirp.Core/DTOs/AuthorDTO.cs
+++ src/Chirp.Core/DTOs/AuthorDTO.cs
@@ -1,5 +1,17 @@
+using FluentValidation;
+
 public class AuthorDTO
 {
 	public required string Name { get; set; }
 	public required string Email { get; set; }
+}
+
+public class AuthorDTOValidator : AbstractValidator<AuthorDTO>
+{
+	public AuthorDTOValidator()
+	{
+		RuleFor(x => x.Email)
+			.EmailAddress()
+			.WithMessage("Email is invalid");
+	}
 }
