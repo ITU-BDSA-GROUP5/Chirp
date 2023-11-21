0b5cdda9dd5aeb2a81b29a9db3c2d160e8ca1bee
Olivier-Baptiste Hansen <oliha@itu.dk>
2023-11-21 23:43:31 +0100

    Remove unused GUIDs and author id on creation

--- src/Chirp.Core/DTOs/CreateCheepDTO.cs
+++ src/Chirp.Core/DTOs/CreateCheepDTO.cs
@@ -1,8 +1,5 @@
-using System.Dynamic;
-
 public class CreateCheepDTO
 {
-	public Guid CheepGuid { get; set; }
 	public required string Text { get; set; }
 	public required string Name { get; set; }
 	public required string Email { get; set; } 
