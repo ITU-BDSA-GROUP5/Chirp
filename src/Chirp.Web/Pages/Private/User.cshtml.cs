70a8857bb76f4f1600741f30830cf6b4bd28d510
johan <johbr@itu.dk>
2023-11-30 14:51:59 +0100

    add about me page to navigation bar

--- src/Chirp.Web/Pages/Private/User.cshtml.cs
+++ src/Chirp.Web/Pages/Private/User.cshtml.cs
@@ -15,6 +15,8 @@ namespace MyApp.Namespace
         public int LastPageNumber { get; set; }
         public string? PageUrl { get; set; }
 
+        public string? Mail { get; set; }
+
 
         public UserModel(IAuthorRepository authorRepository, ICheepRepository cheepRepository)
         {
@@ -22,7 +24,7 @@ namespace MyApp.Namespace
             CheepRepository = cheepRepository;
         }
 
-        public ActionResult OnGet([FromQuery(Name = "page")] int page = 1)
+        public async Task<ActionResult> OnGet([FromQuery(Name = "page")] int page = 1)
         {
             string? name = User.Identity?.Name;
 
@@ -32,6 +34,11 @@ namespace MyApp.Namespace
                 PageNumber = page;
                 LastPageNumber = CheepRepository.GetPageAmount(name);
                 PageUrl = HttpContext.Request.GetEncodedUrl().Split("?")[0];
+
+                string token = User.FindFirst("idp_access_token")?.Value
+                    ?? throw new Exception("Github token not found");
+                    
+                Mail = await GithubHelper.GetUserEmailGithub(token, name);
             }
 
             return Page();
