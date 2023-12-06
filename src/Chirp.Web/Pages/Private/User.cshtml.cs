0f79aa18242796c022dbb6b2ee6e19f1e354c9e1
Philip Han <46556419+Philipnah@users.noreply.github.com>
2023-12-06 13:35:48 +0100

    remove unnecessary using & change css class name
    Co-authored-by: johan <johbr@itu.dk>

--- src/Chirp.Web/Pages/Private/User.cshtml.cs
+++ src/Chirp.Web/Pages/Private/User.cshtml.cs
@@ -2,56 +2,55 @@ using Chirp.Core;
 using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Caching.Memory;
 
 namespace MyApp.Namespace
 {
-    public class UserModel : PageModel
-    {
-        private readonly IAuthorRepository AuthorRepository;
-        private readonly ICheepRepository CheepRepository;
-        public required List<CheepDTO> Cheeps { get; set; }
-        public required List<AuthorDTO> Followees { get; set; }
+	public class UserModel : PageModel
+	{
+		private readonly IAuthorRepository AuthorRepository;
+		private readonly ICheepRepository CheepRepository;
+		public required List<CheepDTO> Cheeps { get; set; }
+		public required List<AuthorDTO> Followees { get; set; }
 
-        public int PageNumber { get; set; }
-        public int LastPageNumber { get; set; }
-        public string? PageUrl { get; set; }
+		public int PageNumber { get; set; }
+		public int LastPageNumber { get; set; }
+		public string? PageUrl { get; set; }
 
-        public string? Mail { get; set; }
+		public string? Mail { get; set; }
 
 
-        public UserModel(IAuthorRepository authorRepository, ICheepRepository cheepRepository)
-        {
-            AuthorRepository = authorRepository;
-            CheepRepository = cheepRepository;
-        }
+		public UserModel(IAuthorRepository authorRepository, ICheepRepository cheepRepository)
+		{
+			AuthorRepository = authorRepository;
+			CheepRepository = cheepRepository;
+		}
 
-        public async Task<ActionResult> OnGet([FromQuery(Name = "page")] int page = 1)
-        {
-            string? name = User.Identity?.Name;
+		public async Task<ActionResult> OnGet([FromQuery(Name = "page")] int page = 1)
+		{
+			string? name = User.Identity?.Name;
 
-            if (name != null)
-            {
-                Cheeps = CheepRepository.GetCheepsFromAuthor(page, name);
-                PageNumber = page;
-                LastPageNumber = CheepRepository.GetPageAmount(name);
-                Followees = AuthorRepository.GetFollowing(name);
-                PageUrl = HttpContext.Request.GetEncodedUrl().Split("?")[0];
+			if (name != null)
+			{
+				Cheeps = CheepRepository.GetCheepsFromAuthor(page, name);
+				PageNumber = page;
+				LastPageNumber = CheepRepository.GetPageAmount(name);
+				Followees = AuthorRepository.GetFollowing(name);
+				PageUrl = HttpContext.Request.GetEncodedUrl().Split("?")[0];
 
-                string token = User.FindFirst("idp_access_token")?.Value
-                    ?? throw new Exception("Github token not found");
+				string token = User.FindFirst("idp_access_token")?.Value
+					?? throw new Exception("Github token not found");
 
-                Mail = await GithubHelper.GetUserEmailGithub(token, name);
-            }
+				Mail = await GithubHelper.GetUserEmailGithub(token, name);
+			}
 
-            return Page();
-        }
+			return Page();
+		}
 
-        //This deletes the user and associated cheeps from the database. Note that it does not log out or change anything azure and cookie related
-        public ActionResult OnPostDelete()
-        {
-            AuthorRepository.DeleteAuthorByName(User.Identity?.Name!);
-            return Redirect("/");
-        }
-    }
+		//This deletes the user and associated cheeps from the database. Note that it does not log out or change anything azure and cookie related
+		public ActionResult OnPostDelete()
+		{
+			AuthorRepository.DeleteAuthorByName(User.Identity?.Name!);
+			return Redirect("/");
+		}
+	}
 }
